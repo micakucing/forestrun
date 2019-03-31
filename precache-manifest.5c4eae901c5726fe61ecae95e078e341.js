4905cb550fcf1f25933bcefa62827d0acb698c66
mica neko <micakucing@gmail.com>
2019-03-31 12:50:33 +0700

    Updates

---
+++ precache-manifest.5c4eae901c5726fe61ecae95e078e341.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00509f5ca20ad7f570d",
+    "url": "/forestrun/static/js/runtime~main.e2cea6f7.js"
+  },
+  {
+    "revision": "93ba814664330fc456f2",
+    "url": "/forestrun/static/js/main.14c9bb23.chunk.js"
+  },
+  {
+    "revision": "9fa8c2b6a9f00a65f49a",
+    "url": "/forestrun/static/js/2.3f258a9a.chunk.js"
+  },
+  {
+    "revision": "93ba814664330fc456f2",
+    "url": "/forestrun/static/css/main.a3bd4ed5.chunk.css"
+  },
+  {
+    "revision": "c0d01f32c09e7adb3d2a0e381e47e5c2",
+    "url": "/forestrun/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e44a25d694a08f19eb426c3b82f804ee.js"
+  "/forestrun/precache-manifest.5c4eae901c5726fe61ecae95e078e341.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/forestrun/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
