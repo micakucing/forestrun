4683bf6f2e8261edd05c9b1549e43e8667289a7f
mica neko <micakucing@gmail.com>
2019-03-31 12:43:29 +0700

    Updates

---
+++ precache-manifest.e44a25d694a08f19eb426c3b82f804ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3fd4072db2f231a96ec4",
+    "url": "/static/js/main.afaebb32.chunk.js"
+  },
+  {
+    "revision": "9fa8c2b6a9f00a65f49a",
+    "url": "/static/js/2.3f258a9a.chunk.js"
+  },
+  {
+    "revision": "3fd4072db2f231a96ec4",
+    "url": "/static/css/main.a3bd4ed5.chunk.css"
+  },
+  {
+    "revision": "0a261fcc0ec5cdb22b8e7c8f51c5c7fa",
+    "url": "/index.html"
+  }
+];
